5367e9f59dd68b2635bb2ea4f01240b8af77b5b2
sjc-bui <bui@setjapan.co.jp>
2021-07-27 18:01:14 +0900

    Using of scroll view.

--- json-cache/ViewControllers/BlankSheetViewController.swift
+++ json-cache/ViewControllers/BlankSheetViewController.swift
@@ -10,20 +10,64 @@ import RxSwift
 
 class BlankSheetViewController: UIViewController {
 
+  let scrollView = UIScrollView()
+  let contentView = UIView()
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.view.backgroundColor = .white
-    example()
+    
+    setupScrollView()
+    setupViews()
   }
 
-  private func example() {
-    let obj = Observable.from(["dog", "monkey", "cat", "shark"])
-    obj.subscribe(onNext: { data in
-      print(data)
-    }, onError: { error in
-      print(error)
-    }, onCompleted: {
-      print("Completed")
-    }).dispose()
+  func setupScrollView(){
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    contentView.translatesAutoresizingMaskIntoConstraints = false
+    
+    view.addSubview(scrollView)
+    scrollView.addSubview(contentView)
+    
+    scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    scrollView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+    scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+    scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    
+    contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+    contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+    contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+    contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
   }
+
+  func setupViews(){
+    contentView.addSubview(label1)
+    label1.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+    label1.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+    label1.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+    
+    contentView.addSubview(label2)
+    label2.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+    label2.topAnchor.constraint(equalTo: label1.bottomAnchor, constant: 25).isActive = true
+    label2.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+    label2.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+  }
+
+  let label1: UILabel = {
+    let label = UILabel()
+    label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.............."
+    label.numberOfLines = 0
+    label.sizeToFit()
+    label.textColor = UIColor.white
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+
+  let label2: UILabel = {
+    let label = UILabel()
+    label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    label.numberOfLines = 0
+    label.sizeToFit()
+    label.textColor = UIColor.white
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
 }
